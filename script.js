dad59a86c4b19e3111cd929fac44712fefbaadb8
Kevin Cowley <cowley.kevin.l@gmail.com>
2016-10-18 22:19:09 -0600

    click lights onOffonOff

--- script.js
+++ script.js
@@ -8,3 +8,49 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+var stopButt = document.querySelector('#stopButton');
+// var redStopLight = document.querySelector('#stopLight')
+// ^^unneeded
+var count=0;
+stopButt.addEventListener('click', function(){
+    count++;
+        //count variable increases each click
+    console.log(count);
+    if (count %2 === 0){
+        //when count = even
+      document.querySelector('#stopLight').style.backgroundColor="black";
+    }
+    else{
+      document.querySelector('#stopLight').style.backgroundColor="red";
+    }
+});
+
+var slowButt = document.querySelector('#slowButton');
+// var yellowStopLight = document.querySelector('#slowLight');
+// ^^unneeded
+slowButt.addEventListener('click', function(){
+  count++;
+      //count variable increases each click
+  console.log(count);
+  if (count %2 === 0){
+      //when count = even
+    document.querySelector('#slowLight').style.backgroundColor="black";
+  }
+  else{
+    document.querySelector('#slowLight').style.backgroundColor="yellow";
+  }});
+
+var goButt = document.querySelector('#goButton');
+// var greenStopLight = document.querySelector('#goLight')
+// ^^unneeded
+goButt.addEventListener('click', function(){
+  count++;
+      //count variable increases each click
+  console.log(count);
+  if (count %2 === 0){
+      //when count = even
+    document.querySelector('#goLight').style.backgroundColor="black";
+  }
+  else{
+    document.querySelector('#goLight').style.backgroundColor="green";
+  }});
